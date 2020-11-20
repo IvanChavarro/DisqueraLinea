ced7953896efc2147b5818bddbc2b59a3277edbc
ktibaquira <55606123+ktibaquira@users.noreply.github.com>
2020-11-20 00:24:36 -0500

    Interfaz administrador

--- src/main/java/com/mycompany/proyectofinallinea1/controller/IndexController.java
+++ src/main/java/com/mycompany/proyectofinallinea1/controller/IndexController.java
@@ -21,13 +21,11 @@ public class IndexController {
      */
     public IndexController() {
     }
-
     public String login() {
         return "faces/login.xhtml";
     }
-
     public String registro() {
         return "faces/registroUser.xhtml";
-
     }
+
 }
